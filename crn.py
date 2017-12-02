c9e07325e73dd00400b8a4ac42e9c733e9ed0428
Willie Kaufman <williekaufman@Willies-MacBook-Pro.local>
2017-12-02 15:27:45 -0600

    fixes

--- crn.py
+++ crn.py
@@ -3,19 +3,29 @@ import math
 import time
 import numpy as np
 
-do_step(reactions, rates, species, volume, log):
-  update_rates(species, reactions, rates, volume):
+reactions = []
+rates = []
+species = dict()
+volume = 1
+log = []
+time = 10000
+
+def do_step():
+  global reactions, rates, species, volume, log, tau
+  update_rates()
   for i in xrange(0, len(rates)):
-	  do_reaction(reactions[i], species, np.random.poisson(1/rates[i]))
+	  do_reaction(np.random.poisson(1/rates[i]))
   log.append(species)
 
-do_reaction(reaction, species, n):
+def do_reaction(n):
+  global reactions, species
   for x in reaction[0].keys():
 	species[x] -= n * reaction[0][x]
   for y in reaction[1].keys():
 	species[x] += n * reaction[1][x]
 
-update_rates(species, reactions, rates, volume):
+def update_rates():
+  global species, reactions, rates, volume
   for i in xrange(0, len(reactions)):
 	reaction = reactions[i]
 	rates[i] = reaction[2] 
@@ -24,4 +34,8 @@ update_rates(species, reactions, rates, volume):
 		rates[i] *= species[y] - j
 		rates[i] /= volume
 
+while (time > 0):
+  do_step()
+  time -= 1
 
+# log is now full of species - graph it somehow
