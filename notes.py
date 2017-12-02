3fef0a0e0e4ebe26d5a3c0bff087a6a0ad6a6d97
williekaufman <williekaufman1@gmail.com>
2017-12-02 15:05:50 -0600

    Delete notes.py

--- notes.py
+++
@@ -1,7 +0,0 @@
-reactions[n][0] =  for the nth reaction dictionary of reactant species: number
-reactions[n][1] = for the nth reaction dictionary of product species: number
-reactions[n][2] = rate constant of nth reaction
-
-species = dictionary of species name: number of particles
-
-rates[n] = current expected number of time steps for reaction n
