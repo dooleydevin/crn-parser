c9aca22ebf60132f65984eaa5d38bd95f623bd01
Devin Dooley <devinadooley@gmail.com>
2017-12-04 22:33:19 -0600

    better looking graph

--- crngraph.py
+++ crngraph.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
 def graph(log):
+    plt.style.use('ggplot')
     fig, ax = plt.subplots()
 
     for specie in log.keys():
-        plt.plot(log[specie])
+        plt.plot(log[specie], label=specie)
 
+    plt.legend(loc='center left', bbox_to_anchor=(1, .5), fancybox=True)
     ax.set(xlabel='Time', ylabel='Concentration')
     ax.grid()
     plt.show()
