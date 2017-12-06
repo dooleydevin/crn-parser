5d25647e42caf089fd7e45e8e4a5fd79aa772557
obsequiousnewt <obsequiousnewt@gmail.com>
2017-12-06 15:47:36 -0600

    cleanup the code, now with more cleanup

--- crn.py
+++ crn.py
@@ -6,61 +6,52 @@ import numpy as np
 from crngraph import graph
 
 example = "oscillator"
-global species
 
 # oscillator
 if (example == "oscillator"):
-  reactions = [[{'A': 1, 'B': 1}, {'B': 2}, 1], [{'B': 1, 'C': 1}, {'C': 2}, 1], [{'C': 1, 'A': 1}, {'A': 2}, 1]]
-  species = {'A': 100, 'B': 100, 'C': 100}
-  rates = [1, 1, 1]
+    reactions = [[{'A': 1, 'B': 1}, {'B': 2}, 0.1],
+               [{'B': 1, 'C': 1}, {'C': 2}, 0.1],
+               [{'C': 1, 'A': 1}, {'A': 2}, 0.1]]
+    species = {'A': 130, 'B': 100, 'C': 70}
 
 # parity
 if (example == "parity"):
-  reactions = [[{'A': 2}, {}, .01]]
-  species = {'A': 101}
-  rates = [1]
+    reactions = [[{'A': 2}, {}, .01]]
+    species = {'A': 101}
+    rates = [1]
 
-volume = 200
+volume = 500
 log = {n: [] for n in species.keys()} 
-time = 1000
+time = 5000
 
 def do_step():
-    global reactions, rates, species, volume, log, tau
-    update_rates()
-    for i in range(0, len(rates)):
-        rate = rates[i]
+    global reactions, rates, species, volume, log
+    for i in reactions:
+        rate = get_reaction_rate(i, species)
         if rate != -1:
-            do_reaction(reactions[i], np.random.poisson(1/rates[i]))
+            do_reaction(i, np.random.poisson(1/rate), species)
 
     for specie in species.keys():
         log[specie].append(species[specie])
 
-
-def do_reaction(reaction, n):
-    global species
+def do_reaction(reaction, n, species):
     for x in reaction[0].keys():
         species[x] -= n * reaction[0][x]
     for y in reaction[1].keys():
         species[y] += n * reaction[1][y]
 
-def update_rates():
-    global species, reactions, rates, volume
-    for i in range(0, len(reactions)):
-        reaction = reactions[i]
-        rates[i] = reaction[2] 
-        for y in reaction[0].keys():
-            for j in range(0, reaction[0][y]):
-                if species[y] - j > 0:
-                    rates[i] = float(rates[i]) / (species[y] - j)
-                    rates[i] *= volume
-                else:
-                    rates[i] = -1
+def get_reaction_rate(reaction, species):
+    rate = reaction[2]
+    for y in reaction[0]:
+        for j in range(reaction[0][y]):
+            if species[y] - j > 0:
+                rate *= volume / (species[y] - j)
+            else:
+                return -1
+    return rate
 
 while (time > 0):
     do_step()
     time -= 1
 
-print(species)
-# log is now full of species - graph it somehow
-print (log)
 graph(log)
