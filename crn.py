b97bcadd26eab5d8941afda82a421684b490642d
obsequiousnewt <obsequiousnewt@gmail.com>
2017-12-06 16:02:28 -0600

    behave like a module, part 2

--- crn.py
+++ crn.py
@@ -3,36 +3,20 @@ import math
 import time
 import numpy as np
 
-from crngraph import graph
+import matplotlib.pyplot as plt
 
 example = "oscillator"
 
 # oscillator
-if (example == "oscillator"):
-    reactions = [[{'A': 1, 'B': 1}, {'B': 2}, 0.1],
+osc_reactions = [[{'A': 1, 'B': 1}, {'B': 2}, 0.1],
                [{'B': 1, 'C': 1}, {'C': 2}, 0.1],
                [{'C': 1, 'A': 1}, {'A': 2}, 0.1]]
-    species = {'A': 130, 'B': 100, 'C': 70}
+osc_species = {'A': 130, 'B': 100, 'C': 70}
 
 # parity
 if (example == "parity"):
     reactions = [[{'A': 2}, {}, .01]]
     species = {'A': 101}
-    rates = [1]
-
-volume = 500
-log = {n: [] for n in species.keys()} 
-time = 5000
-
-def do_step():
-    global reactions, rates, species, volume, log
-    for i in reactions:
-        rate = get_reaction_rate(i, species)
-        if rate != -1:
-            do_reaction(i, np.random.poisson(1/rate), species)
-
-    for specie in species.keys():
-        log[specie].append(species[specie])
 
 def do_reaction(reaction, n, species):
     for x in reaction[0].keys():
@@ -40,7 +24,7 @@ def do_reaction(reaction, n, species):
     for y in reaction[1].keys():
         species[y] += n * reaction[1][y]
 
-def get_reaction_rate(reaction, species):
+def get_reaction_rate(reaction, species, volume):
     rate = reaction[2]
     for y in reaction[0]:
         for j in range(reaction[0][y]):
@@ -50,8 +34,29 @@ def get_reaction_rate(reaction, species):
                 return -1
     return rate
 
-while (time > 0):
-    do_step()
-    time -= 1
+def graph(log):
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
 
-graph(log)
+    for specie in log.keys():
+        plt.plot(log[specie], label=specie)
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, .5), fancybox=True)
+    ax.set(xlabel='Time', ylabel='Concentration')
+    ax.grid()
+    plt.show()
+
+def crn(reactions, species, volume, time):
+    log = {n: [] for n in species.keys()}
+    while (time > 0):
+        for i in reactions:
+            rate = get_reaction_rate(i, species, volume)
+            if rate != -1:
+                do_reaction(i, np.random.poisson(1/rate), species)
+
+        for specie in species.keys():
+            log[specie].append(species[specie])
+        time -= 1
+    graph(log)
+
+crn(osc_reactions, osc_species, 500, 5000)
